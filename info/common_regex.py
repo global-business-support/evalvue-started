3311bb423a5bb1d4affa032a09faa403a50243de
JAYDEEPKARODE <jaydeepkarode20074@acropolis.in>
2024-06-18 11:58:50 +0530

    change in gstin regex
    change in gstin regex

--- info/common_regex.py
+++ info/common_regex.py
@@ -51,7 +51,7 @@ def validate_pin_code(pincode):
     return False
 
 def validate_gstin(gstin):
-    if gstin is None:
+    if gstin is None or len(gstin)<1:
         return True
     if re.match(r"^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[A-Z0-9]{1}Z[A-Z0-9]{1}$",gstin.strip()):
         return True
